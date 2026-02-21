cf37b055ea92c5be462b8e56790e169124ee5b1f
Anderson Lignelli <lignelli77@gmail.com>
2026-02-21 17:33:40 -0300

    update

--- 3_styles_css/src/app/page.js
+++ 3_styles_css/src/app/page.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         {/* 1 -  CSS Global */}
+         {/* 1- CSS Global */}
          <h1>Meu titulo</h1>
          {/* 2- CSS Modules */}
          <h2 className={styles.heading_module}>Meu Css Modules</h2>
          <div className={styles.container}>
           <p>Testando CSS Modules</p>
          </div>
-         {/* 3 - Tailwind CSS */}
+         {/* 3- Tailwind CSS */}
          <div className="bg-blue-500 p-4 rounded-lg">
           <p>Teste Tailwind</p>
          </div>
