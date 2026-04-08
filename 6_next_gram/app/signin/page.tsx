10ddd2f7b5193a6c7ded8c0e61866c9d002a8d87
Anderson Lignelli <lignelli77@gmail.com>
2026-04-07 23:26:52 -0300

    update

--- 6_next_gram/app/signin/page.tsx
+++ 6_next_gram/app/signin/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { signIn, providerMap } from 'auth'
+import type { ReactNode } from 'react'
 
-const SignInPage = () => {
-  return (
-    <div>SignInPage</div>
-  )
+import { BsGoogle } from 'react-icons/bs'
+
+const providerIcons: Record<string, ReactNode> = {
+  Google: <BsGoogle />,
 }
 
+const icons = [{name: 'Google', icon: <BsGoogle />}]
+
+const SignInPage = async () => {
+
+
+return <h2>Acesse ou crie sua conta com uma das opcoes disponiveis</h2>
+
+{ Object.values(providerMap).map((provider) => {
+   return (
+     <form
+      key={provider.id}
+       action={async () => {
+      "use server";
+      await signIn(provider.id, { redirectTo: "/" })
+
+}}>
+  <button>
+    <span>Entrar com <strong>{provider.name}</strong></span>
+  </button>
+
+</form>
+
+    )})}
+} 
+
 export default SignInPage
+
